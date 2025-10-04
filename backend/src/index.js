a89aeb2cd5f0b8605bcb16bb381e6005a20b069a
AndresMorquecho <morquechoisrael@hotmail.com>
2025-10-04 14:15:51 -0500

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
